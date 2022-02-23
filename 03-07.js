51a8d5672fd062e0baa24dfd8828b63269968331
MatyasVizer <matyasvizer@gmail.com>
2022-02-23 12:36:17 +0100

    completed 03-07

---
+++ 03-07.js
@@ -0,0 +1,25 @@
+//create arbitrary objects
+
+const myCar1 = {
+    brand: "Volkswagen",
+    generation: "Mk7",
+    model: "Golf",
+    edition: "Trendline",
+    displacement: 1.2,
+    cylinders: 4,
+    drivetrain: "FWD",
+    horsepower: 110,
+    turbo: "yes" 
+}
+
+const myCar2 = {
+    brand: "BMW",
+    generation: "E60",
+    model: "5 Series",
+    edition: "M",
+    displacement: 5,
+    cylinders: 10,
+    drivetrain: "RWD",
+    horsepower: 750,
+    turbo: "yes" 
+}
